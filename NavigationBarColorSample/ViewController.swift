b6036864487f554278aa1f783acf345206a84c0d
tomotakashimizu <tomotaka.developer@gmail.com>
2020-06-18 05:58:43 +0900

    Initial Commit

---
+++ NavigationBarColorSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationBarColorSample
+//
+//  Created by 清水智貴 on 2020/06/18.
+//  Copyright © 2020 tomotaka.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
