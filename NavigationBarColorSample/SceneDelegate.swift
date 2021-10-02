0d0de50d8c857f98b4f5b91e602ffe8ca0941f98
tomotakashimizu <tomotaka.developer@gmail.com>
2021-10-02 21:13:49 +0900

    [update] navigation bar color の変更
    iOS15 以降 navigation bar の背景色が透明に変更になったため

--- NavigationBarColorSample/SceneDelegate.swift
+++ NavigationBarColorSample/SceneDelegate.swift
@@ -21,18 +21,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // 全てのNavigationControllerのそれぞれの色を変更する場合ここに書く
         // 個別に色を変えたい場合、そのViewControllerにコードを書くことで個別に色を変えられる
         // ナビゲージョンアイテムの文字色
-        UINavigationBar.appearance().tintColor = UIColor.yellow
+        UINavigationBar.appearance().tintColor = UIColor.red
 
         UINavigationBar.appearance().titleTextAttributes =
             // ナビゲーションバーのタイトルの文字色
-            [.foregroundColor: UIColor.white,
+            [.foregroundColor: UIColor.blue,
              // フォントの種類
              .font: UIFont(name: "Times New Roman",
                            // フォントサイズ
                            size: 27)!]
 
         // ナビゲーションバーの背景色
-        UINavigationBar.appearance().barTintColor = UIColor.red
+        //UINavigationBar.appearance().barTintColor = UIColor.red
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
